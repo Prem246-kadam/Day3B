69820d6ec6d1a8548c93bc68595e8c1b8ff6f380
Prem246-kadam <127394840+Prem246-kadam@users.noreply.github.com>
2023-07-07 11:43:57 +0530

    Create Constructure.cs

---
+++ Constructure.cs
@@ -0,0 +1,22 @@
+// Default Constructor:
+public class MyClass
+{
+  
+    public MyClass()
+    {
+        // code here
+    }
+}
+
+//Parameterized Constructor
+public class MyClass
+{
+    // Parameterized constructor
+    public MyClass(int value1, int value2)
+    {
+        // Initialization code using the provided values
+    }
+}
+//copy constructure
+
+
